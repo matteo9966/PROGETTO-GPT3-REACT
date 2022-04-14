c644dfc2b758dd84f5662e808dea1ebd69cb26c2
matteo9966 <matteoluigitommasi@gmail.com>
2022-04-14 17:04:26 +0200

    [HEADER+BRAND] added two new components

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,7 +1,23 @@
-import styles from './brand.module.css';
-export const Brand= () => {
-    return (
-      <div>brand</div>
-    )
-  }
-  
+import styles from "./brand.module.css";
+import { google, slack, atlassian, dropbox, shopify } from "./import";
+export const Brand = () => {
+  return (
+    <div className={`${styles["gpt3__brand"]} section__padding`}>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import styles from './header.module.css'
-
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png';
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className={`${styles['gpt3__header']} section__padding`} id="home">
+      <div className={styles['gpt3__header-content']}>
+        <h1 className={"gradient__text"}>
+          Let's Build something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, ratione.</p>
+        <div className={styles['gpt3__header-content__input']}>
+          <input type="email" placeholder='your email...' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className={styles['gpt3__header-content__people']}>
+          <img src={people} alt="people" />
+          <p>1600 people registered in the past 24 hours</p>
+        </div>
+      </div>
+        <div className={styles['gpt3__header-image']}>
+          <img src={ai} alt="ai" />
+        </div>
+    </div>
   )
 }
