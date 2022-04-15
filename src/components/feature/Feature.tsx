e911af9fe61afe9b704f0e706f917dfbf65f2dc5
matteo9966 <matteoluigitommasi@gmail.com>
2022-04-15 18:09:29 +0200

    fix bug di css modules

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,7 +1,19 @@
-import styles from './feature.module.css';
-export const Feature = () => {
-    return (
-      <div>Article</div>
-    )
-  }
-  
+import * as styles from "./feature.module.css";
+import './feature.module.css'
+export const Feature: React.FC<{ title: string; text: string }> = (props) => {
+  return (
+    <div className={"gpt3__features-container__feature"}>
+      <div className={"gpt3__features-container__feature-title"}>
+        <div />
+        <h1>{props.title}</h1>
+      </div>
+
+      <div className={"gpt3__features-container_feature-text"}>
+        <p>
+
+        {props.text}
+        </p>
+      </div>
+    </div>
+  );
+};
